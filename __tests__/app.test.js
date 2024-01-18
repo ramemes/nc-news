5819a0123fb88eb81535ac7cd20ffddb3ab134b0
ramemes <17809655+ramemes@users.noreply.github.com>
2024-01-18 11:22:54 +0000

    complete task 9

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -257,35 +257,31 @@ describe("POST /api/articles/:article_id/comments", () => {
     })
 })
 
-// describe("DELETE /api/comments/:comment_id", () => {
-//     test("responds with status 204 and no content" , () => {
-//         return request(app).delete('/api/comments/1')
-//         .expect(204)
-//         .then(({body}) => {
-//             expect(body).toEqual({})
-//         })
-//     })
-//     test("returns 404 error if comment_id doesnt exist", () => {
-//         return request(app).post('/api/comments/1203')
-//         .expect(404)
-//         .then(({ body }) => {
-//             expect(body.msg).toBe(`comment_id with value 1203 does not exist in comments`)   
+describe("DELETE /api/comments/:comment_id", () => {
+    test("responds with status 204 and no content" , () => {
+        return request(app).delete('/api/comments/1')
+        .expect(204)
+        .then(({body}) => {
+            expect(body).toEqual({})
+        })
+    })
+    test("returns 404 error if comment_id doesnt exist", () => {
+        return request(app).delete('/api/comments/1203')
+        .expect(404)
+        .then(({ body }) => {
+            expect(body.msg).toBe(`comment_id with value 1203 does not exist in comments`)   
 
-//         })
-//     })
+        })
+    })
+    test("returns 400 error for incorrect format given", () => {
+        return request(app).delete('/api/comments/awdas')
+        .expect(400)
+        .then(({ body }) => {
+            expect(body.msg).toBe("invalid format")   
 
-// })
+        })
+    })
+
+})
 
 
-
-// Description
-// Should:
-
-// be available on /api/comments/:comment_id.
-// delete the given comment by comment_id.
-// Responds with:
-
-// status 204 and no content.
-// Consider what errors could occur with this endpoint, and make sure to test for them.
-
-// Remember to add a description of this endpoint to your /api endpoint.
