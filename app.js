6e05bda9262ccdb62913e0c840efd375a17a9362
ramemes <17809655+ramemes@users.noreply.github.com>
2024-02-05 10:12:54 +0000

    add CORS

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors')
 
 const apiRouter = require('./routes/api-router')
 
 const app = express();
-app.use(express.json())
 
+app.use(cors());
+app.use(express.json());
 
-app.use('/api', apiRouter)
+app.use('/api', apiRouter);
 
 
 app.use((err, req, res, next) => {
